3c9fef05233324e7d7fd613d6d339be715a63513
Francisco Filipe <francilipe.7@gmail.com>
2018-05-26 19:40:39 +0100

    trying to fix collision when number of divisions of the terrain is low

--- TP6/code/LightingScene.js
+++ TP6/code/LightingScene.js
@@ -128,7 +128,7 @@ class LightingScene extends CGFscene {
 
 		this.car = new MyVehicle(this, 10, 1.3, 12);
 		this.crane = new MyCrane(this, 'UP', 'D');
-		this.terrain = new MyTerrain(this, 48, 50.0, 50.0, 0, 5, 0, 5, myAltimetry, this.terrainAppearances[this.currTerrainAppearance]);
+		this.terrain = new MyTerrain(this, 8, 50.0, 50.0, 0, 5, 0, 5, myAltimetry1, this.terrainAppearances[this.currTerrainAppearance]);
 
 		this.platform = new MyQuad(this);
 		this.planeZ = 17;
